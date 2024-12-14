809fb2fd182a2404babd9b8b7a3d6c5fa6479fa1
sidratul <muhammad.mochi@gmail.com>
2024-12-15 03:21:59 +0700

    add log in router

--- src/app/api/users/profile/route.ts
+++ src/app/api/users/profile/route.ts
@@ -2,24 +2,29 @@ import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const BE_URL = process.env.BE_URL+`/fetch-user-data`;
-  console.log('get called')
   const res = await fetch(BE_URL, {
     headers: getHeader(req),
     method: 'GET',
+  }).catch((err) => {
+    console.log('error', err.message);
+    return Response.json({ message: 'Internal Server Error' }, {status: 500});  
   })
+
   const data = await res.json();
   return Response.json({ data }, {status: res.status});
 }
 
 export async function PATCH(req: NextRequest) {
   const BE_URL = process.env.BE_URL+`/update-user-data`;
-  console.log('get called UPDATE')
 
   const body = await req.json();
   const res = await fetch(BE_URL, {
     headers: getHeader(req),
     method: 'PATCH',
     body: JSON.stringify(body),
+  }).catch((err) => {
+    console.log('error', err.message);
+    return Response.json({ message: 'Internal Server Error' }, {status: 500});  
   })
   
   const data = await res.json();
